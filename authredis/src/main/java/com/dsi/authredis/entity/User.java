26965745f521e9fa66a2a8c118b2be56fd34b877
sadik.hasan@dsinnovators.com <sadik.hasan@dsinnovators.com>
2024-07-16 16:04:08 +0600

    added login home pages

--- authredis/src/main/java/com/dsi/authredis/entity/User.java
+++ authredis/src/main/java/com/dsi/authredis/entity/User.java
@@ -13,5 +13,38 @@ import java.util.stream.Collectors;
 @Entity
 @Table(name = "user_info")
 public class User extends BaseEntity {
-    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- authredis/src/main/java/com/dsi/authredis/service/UserService.java
+++ authredis/src/main/java/com/dsi/authredis/service/UserService.java
@@ -1,6 +1,5 @@
 package com.dsi.authredis.service;
 
-
 import com.dsi.authredis.entity.User;
 import com.dsi.authredis.repository.UserRepository;
 import jakarta.ejb.EJB;
@@ -10,26 +9,23 @@ import java.util.Optional;
 
 @Stateless
 public class UserService {
+
     @EJB
     private UserRepository userRepository;
 
     public Optional<User> getUserByEmail(String email) {
-        Optional<User> user = userRepository.findByEmail(email);
         return userRepository.findByEmail(email);
     }
 
     public Optional<User> getUserByPhoneNumber(String phoneNumber) {
-        Optional<User> user = userRepository.findByPhoneNumber(phoneNumber);
-        return user;
+        return userRepository.findByPhoneNumber(phoneNumber);
     }
 
     public Optional<User> getUserByPhoneNumberAndPassword(String phoneNumber, String password) {
-        Optional<User> user = userRepository.findByPhoneNumberAndPassword(phoneNumber, password);
-        return user;
+        return userRepository.findByPhoneNumberAndPassword(phoneNumber, password);
     }
 
     public Optional<User> getUserByEmailAndPassword(String email, String password) {
-        Optional<User> user = userRepository.findByEmailAndPassword(email, password);
         return userRepository.findByEmailAndPassword(email, password);
     }
 }
